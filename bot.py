afec0150c174a54b90b131399cec3a027b7288ac
XYFC128 <xyfc128@gmail.com>
2021-08-13 08:36:31 +0800

    basic connection

--- bot.py
+++ bot.py
@@ -1,1 +1,13 @@
- 
+#!/usr/bin/python3
+import os
+
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+with open('token.txt', 'r') as f:
+    client.run(f.read())
