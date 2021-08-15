6c196b0d5f3ed12c079a7108a76350aa5b69c54e
XYFC128 <xyfc128@gmail.com>
2021-08-15 23:01:58 +0800

    全新浪漫技巧

--- romantic.py
+++ romantic.py
@@ -1,41 +1,52 @@
 from utils import *
 import json
+import xml.etree.ElementTree as ET
 
-romantic_data = None
-with open('data/romantic_states.json', 'r') as f:
-    romantic_data=json.loads(f.read())
+roman_data = ET.parse('data/romantic_states.xml')
+root = roman_data.getroot()
 
-
-def step_options(anwser: str, stack: list):
-    new_options = romantic_data[anwser]['options']
-    display = romantic_data[anwser]['print']
-    if len(new_options) > 0:
-        stack.append(RomanticInternalState(new_options))
-        display += '\n你可以輸入: '
-        for opt in  new_options:
-            display += '\n' + opt
-    stack.append(PrintState(display))
-
-
-class RomanticInternalState:
-    def __init__(self, options: list, re_enter_count=0) -> None:
+class RomanticState:
+    def __init__(self, cur_node: ET.Element, inter=None) -> None:
+        self.node = cur_node
+        displays = []
+        options = {}
+        for child in self.node:
+            if child.tag == 'display':
+                if child.attrib['type'] == 'normal':
+                    displays.append(child.text)
+            elif child.tag == 'node':
+                options[child.attrib['name']] = child
+        
+        self.displays = displays
         self.options = options
-        self.re_enter_count = re_enter_count
-    
-    
+        self.inter = inter
+
+    def selected_handler(self, inter, stack):
+        selected = [option.label for option in inter.select_menu.selected_options]
+        ans = selected[0]
+        node = self.options[ans]
+        stack.append(RomanticState(node, inter))
+
+
     def run(self, message: Message, user_stack: list):
-        anwser = message.content
-        if anwser in self.options:
-            step_options(anwser, user_stack)
-        else:
-            self.re_enter_count+=1
-            user_stack.append(self)
-            user_stack.append(PrintState('請重新輸入' + '!' * self.re_enter_count))
+        if len(self.options) > 0:
+            options = [opt for opt in self.options]
+            user_stack.append(MenuState('輸入選單：', options, self.selected_handler))
+
+        self.displays = self.displays[::-1]
+        if len(self.displays) > 0:
+            first_display = None
+            if self.inter != None:
+                first_display = self.displays.pop()
+            for display in self.displays:
+                user_stack.append(PrintState(display))
+            if first_display != None:
+                user_stack.append(PrintState(first_display, inter=self.inter))
 
 
     def require_input(self):
-        return True
+        return False
 
 
 def romantic_command_handler(channel: TextChannel, args: list, user_stack: list):
-    step_options("問題類型", user_stack)
+    user_stack.append(RomanticState(root))
